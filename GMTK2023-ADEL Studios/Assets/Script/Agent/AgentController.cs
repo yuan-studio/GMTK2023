b7687f8bf50d085a2e6d7ae25c1aa42e0f551169
unfocusedlex <138841477+yuan-studio@users.noreply.github.com>
2023-07-08 21:59:26 +0700

    FInal Agent

--- GMTK2023-ADEL Studios/Assets/Script/Agent/AgentController.cs
+++ GMTK2023-ADEL Studios/Assets/Script/Agent/AgentController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.XR;
@@ -9,8 +10,10 @@ public class AgentController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent Agent;
     [SerializeField] private string decisionTag;
+    [SerializeField] private string obstacleTag;
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private DIRECTION initialDirection;
+    [SerializeField] private STATES initialState;
 
     [SerializeField] private float raycastRange = 8f;
     [SerializeField] private float decisionTime = 5f;
@@ -31,21 +34,23 @@ public class AgentController : MonoBehaviour
         RIGHT
     }
 
-    STATES currentState = STATES.WALKING;
+    STATES currentState;
     STATES previousState;
 
     Vector3 targetPosition;
 
-    private float decisionTimer;
-    private bool enteredDecisionPoint = false;
+    private float decisionTimer = 0f;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.transform.tag);
         if (other.transform.CompareTag(decisionTag))
         {
             Agent.SetDestination(transform.position + targetPosition * 1f);
             ChangeState(STATES.PENDING);
+        }
+        else if (other.transform.CompareTag(obstacleTag) && currentState.Equals(STATES.WALKING))
+        {
+            reverseDirection();
         }
     }
 
@@ -76,6 +81,11 @@ public class AgentController : MonoBehaviour
         previousState = currentState;
     }
 
+    private void reverseDirection()
+    {
+        targetPosition = -targetPosition;
+    }
+
     private bool CanWalkTowards()
     {
         Vector3 reverseDirection = -targetPosition;
@@ -98,6 +108,7 @@ public class AgentController : MonoBehaviour
     }
     private void Start()
     {
+        currentState = initialState;
         previousState = currentState;
         
         switch (initialDirection)
@@ -126,7 +137,6 @@ public class AgentController : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(currentState.ToString());
         if (currentState != previousState)
         {
             OnStateChange();
@@ -134,12 +144,12 @@ public class AgentController : MonoBehaviour
 
         switch (currentState)
         {
-
+            case STATES.IDLE:
+                break;
 
             case STATES.PENDING:
                 if (Agent.remainingDistance <= 0.1f)
                 {
-                    Debug.Log("Arrived at decision point");
                     ChangeState(STATES.DECISION);
                 }
                 break;
@@ -150,6 +160,7 @@ public class AgentController : MonoBehaviour
                 break;
 
             case STATES.DECISION:
+                Debug.Log(decisionTimer);
                 decisionTimer -= Time.deltaTime;
                 //if there's an open path forward, left, or right
                 if (CanWalkTowards())
@@ -166,12 +177,6 @@ public class AgentController : MonoBehaviour
                 break;
         }
         //state changes and checks happen here
-        //if player enters decision point
-        if (enteredDecisionPoint)
-        {
-            
-            enteredDecisionPoint = false;
-        }
 
     }
 }
