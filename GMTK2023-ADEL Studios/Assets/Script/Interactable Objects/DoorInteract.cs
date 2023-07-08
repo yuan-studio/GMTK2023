d5d285958fca6df733d695859bc1a0bdb446a637
unfocusedlex <138841477+yuan-studio@users.noreply.github.com>
2023-07-08 22:56:16 +0700

    Added door

---
+++ GMTK2023-ADEL Studios/Assets/Script/Interactable Objects/DoorInteract.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DoorInteract : Interactable
+{
+    [SerializeField] private float openAmount = 5f;
+    [SerializeField] private float openDuration = 1f;
+
+    [SerializeField] private bool isOpen = false;
+    private bool animating = false;
+
+    private IEnumerator ToggleDoor()
+    {
+        animating = true;
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition;
+        if (isOpen)
+        {
+            targetPosition = startPosition - new Vector3(0f, openAmount, 0f);
+        }
+        else
+        {
+            targetPosition = startPosition + new Vector3(0f, openAmount, 0f);
+        }
+         
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < openDuration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / openDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        animating = false;
+    }
+
+
+
+    public override void Interact()
+    {
+        if (!animating)
+        {
+            StartCoroutine(ToggleDoor());
+            isOpen = !isOpen;
+        }
+    }
+}
